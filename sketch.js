e75a61e850bc0775da26f516d664d3671e94f42e
Alfredo <alfredotvr@users.noreply.github.com>
2026-04-10 21:32:09 -0400

    project scaffolding, first pixel on canvas

---
+++ sketch.js
@@ -0,0 +1,8 @@
+const canvas = document.getElementById('canvas');
+const ctx = canvas.getContext('2d');
+
+canvas.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+ctx.fillStyle = 'white';
+ctx.fillRect(40, 40, 8, 8);
